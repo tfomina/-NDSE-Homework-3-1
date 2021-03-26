8c8be72e42caf7e22b0600554c4a636335788649
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-03-26 19:09:33 +0300

    Вынесла вспомогательные функции в helpers, реализация логирования

---
+++ helpers.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const fs = require("fs");
+
+const FOLDER_NAME = "log";
+
+const validateFilename = (filename) => {
+  let error = "";
+
+  if (!filename) {
+    error = `Имя файла не может быть пустым. Введите другое имя файла.`;
+  }
+
+  const filePath = getFilePath(filename);
+  if (fs.existsSync(filePath)) {
+    error = `Файл с именем ${filePath} уже существует. Введите другое имя файла.`;
+  }
+
+  return error;
+};
+
+const getFilePath = (filename) =>
+  path.join(__dirname, FOLDER_NAME, `${filename}.txt`);
+
+const getDirPath = () => path.join(__dirname, FOLDER_NAME);
+
+const handleError = (error) => {
+  console.error(error);
+  process.exit(-1);
+};
+
+const generateRandomNumFromArr = (arr) =>
+  arr[Math.floor(Math.random() * arr.length)].toString();
+
+module.exports = {
+  validateFilename,
+  getFilePath,
+  handleError,
+  generateRandomNumFromArr,
+  getDirPath,
+};
--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 const readline = require("readline");
+const {
+  getFilePath,
+  validateFilename,
+  handleError,
+  generateRandomNumFromArr,
+  getDirPath,
+} = require("./helpers");
 
 const input = readline.createInterface(process.stdin);
 
-yargs(hideBin(process.argv))
+const POSSIBLE_ANSWERS = ["1", "2"];
+
+const options = yargs(hideBin(process.argv))
   .option("fn", {
     alias: "filename",
     demandOption: true,
     describe: "Название файла для записи лога игры",
     type: "string",
+  })
+  .check((argv) => {
+    const error = validateFilename(argv.filename);
+
+    if (!!error) {
+      handleError(error);
+    } else {
+      return true;
+    }
   })
   .fail((msg, err, yargs) => {
     if (err) throw err;
@@ -22,20 +41,46 @@ yargs(hideBin(process.argv))
   })
   .help().argv;
 
-const generateRandomNum = () => Number(Math.random() < 0.5) + 1;
+process.stdout.write(`Введите ответ 1 (орёл) или 2 (решка)\n`);
+input.on("line", (answer) => {
+  const randomNum = generateRandomNumFromArr(POSSIBLE_ANSWERS);
 
-process.stdout.write(`Введите ответ 1 (орел) или 2 (решка)\n`);
-input.on("line", (val) => {
-  const randomNum = +generateRandomNum();
-  const answer = Number(val);
-
-  if (!answer || ![1, 2].includes(answer)) {
-    console.error("Ошибка! Введено некорректное значение!");
-    process.exit(-1);
+  if (!answer || !POSSIBLE_ANSWERS.includes(answer)) {
+    handleError("Ошибка! Введено некорректное значение!");
   }
 
   const result = randomNum === answer ? "Угадано верно" : "Не угадано";
-  const output = `Число, загаданное компьютером: ${randomNum}. Число, введенное пользователем: ${answer}. ${result}.`;
+  const output = `Число, загаданное компьютером: ${randomNum}. Число, введенное пользователем: ${answer}. ${result}.\n`;
 
   console.log(output);
+
+  const dirPath = getDirPath();
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, (err) => {
+      if (err) handleError("Ошибка создания директории!");
+    });
+  }
+
+  const filePath = getFilePath(options.filename);
+
+  const date = new Date().toLocaleString("ru-RU");
+  const logOutput = `${date}. ${output}`;
+
+  if (fs.existsSync(filePath)) {
+    fs.appendFileSync(filePath, logOutput, (err) => {
+      if (err) {
+        handleError("Ошибка логирования!");
+      }
+    });
+  } else {
+    fs.writeFileSync(
+      filePath,
+      `Лог от ${new Date().toLocaleDateString("ru-RU")}.\n${logOutput}`,
+      (err) => {
+        if (err) {
+          handleError("Ошибка логирования!");
+        }
+      }
+    );
+  }
 });
